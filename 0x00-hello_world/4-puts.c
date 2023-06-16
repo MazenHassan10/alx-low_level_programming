a9fbcd0b350826430216a21c57069f33eb864733
MazenHassan10 <mazen0000072@gmail.com>
2023-06-15 18:43:32 -0700

    oh shit again

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ * 
+ * Description : A C prgram that prints with put function
+ *
+ * Return : Always 0 (Success)
+*/
+int main(void){
+	put("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
