f4afee3820055139739bf7432801b6eaee3fac06
MazenHassan10 <mazen0000072@gmail.com>
2023-10-11 14:18:58 -0700

    OKAY

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef -object_like_macro.h
-#define -object_like_macro.h
+#ifndef 0_OBJECT_LIKE_MACRO_H
+#define 0_OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif
+#endif /* 0_OBJECT_LIKE_MACRO_H */
