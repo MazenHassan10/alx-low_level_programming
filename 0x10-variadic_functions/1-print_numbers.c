5b3e077f63c952a345fa1467eedfa20b18136bba
MazenHassan10 <mazen0000072@gmail.com>
2023-10-12 17:50:37 -0700

    we are near

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,6 +14,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		va_list args;
 		unsigned int i;
 
+		if (!n)
+		{
+			printf("\n");
+			return;
+		}
 		va_start(args, n);
 
 		if (separator != NULL)
