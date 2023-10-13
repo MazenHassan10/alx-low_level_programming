df8574285e1069082d1dd96a35b0a7afb09dca31
MazenHassan10 <mazen0000072@gmail.com>
2023-10-12 18:39:46 -0700

    we are near

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_strings - print strings with seperators
+ * @separator : the sep
+ * @n : number of arguments
+ * Return: the sentence
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+		va_list args;
+		char *str;
+		unsigned int i;
+
+		if (!n)
+		{
+			printf("\n");
+			return;
+		}
+		va_start(args, n);
+
+		if (separator != NULL)
+		{
+			for (i = 0 ; i < n ; i++)
+			{
+				if (i == (n - 1))
+				{
+					printf("%s\n", (str = va_arg(args, char *)) ? str : "(nil)");
+				}
+				else
+				{
+	printf("%s", (str = va_arg(args, char *)) ? str : "(nil)");
+	printf("%s", separator ? separator : NULL);
+				}
+			}
+		}
+		va_end(args);
+}
