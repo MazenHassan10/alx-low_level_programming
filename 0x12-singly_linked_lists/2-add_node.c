5d1564fce845b9e4d6bc7a614177ceb9f46b5ea4
MazenHassan10 <mazen0000072@gmail.com>
2023-10-19 17:23:16 -0700

    here we go

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,18 @@
 #include <string.h>
 #include <stdio.h>
 /**
+ * strlength - get the length of the string
+ * @h : the actual length
+ * Return: the length of the string 
+ */
+int strlength(const char *h)
+{
+	int i = 0;
+	while (h[i] != '\0')
+		i++;
+	return (i);
+}
+/**
  * add_node - adds a new elemnt
  * @head : the header list we gonna use
  * @str : the string we gonna keep
@@ -12,12 +24,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *p = malloc(sizeof(list_t));
 
-	p->str = strdup(str);
-	p->next = *head;
-	*head = p;
 	if (p == NULL)
 	{
-		return (NULL);
+	return (NULL);
 	}
+	p->str = strdup(str);
+	p->len = strlength(str);
+	p->next = *head;
+	*head = p;
 	return (p);
 }
