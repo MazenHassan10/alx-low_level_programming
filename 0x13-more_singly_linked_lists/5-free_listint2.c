a5a0c020257a3aa2e5c854d88494cc1e7fcc5ba6
MazenHassan10 <mazen0000072@gmail.com>
2023-10-24 03:27:10 +0300

    here we go

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2 - free all the list
+ * @head : the header of the list
+ * Return: return nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p = head, *NewP;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (p)
+	{
+		NewP = p->next;
+		free(p);
+		p = NewP;
+	}
+	*head = NULL;
+}
