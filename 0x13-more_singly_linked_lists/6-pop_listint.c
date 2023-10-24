08d5283591fcd49e0e73693ad93e05f318236738
MazenHassan10 <mazen0000072@gmail.com>
2023-10-24 03:56:07 +0300

    here we go

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * pop_listint - free the head
+ * @head : the header of the list
+ * Return: return nothing
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *p = *head;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	*head = (*head)->next;
+	p->next = NULL;
+	return (p->n);
+}
