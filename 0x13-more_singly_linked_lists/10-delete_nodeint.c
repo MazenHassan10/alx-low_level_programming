73da0c73d01ddf4adc079019d7b54c6d88b166fc
MazenHassan10 <mazen0000072@gmail.com>
2023-10-25 04:30:46 +0300

    here we go

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 	p = *head;
 	i = 0;
@@ -39,5 +39,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 		p = p->next;
 	}
-	return (0);
+	return (-1);
 }
