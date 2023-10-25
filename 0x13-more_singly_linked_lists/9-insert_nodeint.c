4d40b0dfb24f1bb9efda538a58d7e172209a503a
MazenHassan10 <mazen0000072@gmail.com>
2023-10-25 03:21:50 +0300

    here we go

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,6 +3,23 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ * listint_len - print the length of the list
+ * @h : thhe header of the list
+ * Return: return list length
+ */
+unsigned int listint_length(const listint_t *h)
+{
+	int i = 0;
+	const listint_t *p = h;
+
+	while (p)
+	{
+		p = p->next;
+		i++;
+	}
+	return (i);
+}
+/**
  * insert_nodeint_at_index - put node in a specific index
  * @head : the header of the list
  * @idx : number of the node
@@ -11,9 +28,8 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *p;
-	listint_t *Node = malloc(sizeof(listint_t));
+	unsigned int i, k;
+	listint_t *Node = malloc(sizeof(listint_t)), *p = *head;
 
 	if (Node == NULL)
 	{
@@ -25,8 +41,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	Node->n = n;
-	p = *head;
 	i = 0;
+	k = listint_length(*head);
+	if (idx < k || (idx == 0 && k == 0))
+	{
 	while (p)
 	{
 		if (idx == 0)
@@ -42,6 +60,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		i++;
 		p = p->next;
+	}
 	}
 	free(Node);
 	return (NULL);
