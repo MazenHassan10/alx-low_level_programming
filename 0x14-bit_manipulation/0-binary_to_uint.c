aeaa6e04e0822d9f18aad955426e6cdf4c3d0088
MazenHassan10 <mazen0000072@gmail.com>
2023-10-27 04:23:45 +0200

    here we go

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * fun - gets the power
+ * k : our number
+ * Return: power of the number
+ */
+unsigned int fun(unsigned int k)
+{
+	unsigned int num = 1;
+
+	while (k--)
+	{
+	num *= 2;
+	}
+	return (num);
+}
+/**
+ * binary_to_uint - print the binary number
+ * b : our number
+ * Return: returns the unit
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, z, bin;
+
+	if (b == NULL)
+	return (0);
+	i = z = bin = 0;
+	while (b[i])
+		i++;
+	while (i--)
+	{
+		switch (b[i])
+		{
+			case '1':
+			bin += fun(z);
+			break;
+			case '0':
+			bin += 0;
+			break;
+			default:
+			return (0);
+		}
+	z++;
+	}
+	return (bin);
+}
